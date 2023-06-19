b5c095ff96ce66af2c35e51cef0f9e6cbd11e9c8
Nimet Okur <okurnimet45@gmail.com>
2023-06-20 00:18:33 +0300

    updated

--- lib/screens/add_event.dart
+++ lib/screens/add_event.dart
@@ -1,200 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
 
-class AddEvent extends StatefulWidget {
-  final DateTime firstDate;
-  final DateTime lastDate;
-  final DateTime? selectedDate;
+class AddEventPage extends StatelessWidget {
+  final Function(String, String, DateTime) onEventAdded;
 
-  const AddEvent({
-    Key? key,
-    required this.firstDate,
-    required this.lastDate,
-    this.selectedDate,
-  }) : super(key: key);
+  AddEventPage({required this.onEventAdded});
 
-  @override
-  State<AddEvent> createState() => _AddEventState();
-}
-
-class _AddEventState extends State<AddEvent> {
-  late DateTime _selectedDate;
-  late TimeOfDay _selectedTime;
-  final _titleController = TextEditingController();
-  final _descController = TextEditingController();
-  final _reminderTimeController = TextEditingController();
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-  FlutterLocalNotificationsPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedDate = widget.selectedDate ?? DateTime.now();
-    _selectedTime = TimeOfDay.now();
-    var initializationSettingsAndroid =
-    AndroidInitializationSettings('@mipmap/ic_launcher');
-    var initializationSettingsIOS = IOSInitializationSettings();
-    var initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid,
-      iOS: initializationSettingsIOS,
-    );
-    flutterLocalNotificationsPlugin.initialize(initializationSettings);
-    tz.initializeTimeZones();
-  }
+  TextEditingController _eventTitleController = TextEditingController();
+  TextEditingController _eventNotesController = TextEditingController();
+  DateTime selectedTime = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Etkinlik Ekle")),
-      body: ListView(
+      appBar: AppBar(
+        title: Text('Add Event'),
+      ),
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: InputDatePickerFormField(
-                  firstDate: widget.firstDate,
-                  lastDate: widget.lastDate,
-                  initialDate: _selectedDate,
-                  onDateSubmitted: (date) {
-                    setState(() {
-                      _selectedDate = date;
-                    });
-                  },
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _eventTitleController,
+              decoration: InputDecoration(
+                labelText: 'Title',
               ),
-              IconButton(
-                icon: Icon(Icons.access_time),
-                onPressed: () async {
-                  final TimeOfDay? picked = await showTimePicker(
-                    context: context,
-                    initialTime: _selectedTime,
-                  );
-                  if (picked != null) {
-                    setState(() {
-                      _selectedTime = picked;
-                    });
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _eventNotesController,
+              decoration: InputDecoration(
+                labelText: 'Notes',
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: TextEditingController(
+                text: '${selectedTime.hour}:${selectedTime.minute.toString().padLeft(2, '0')}',
+              ),
+              readOnly: true,
+              onTap: () {
+                showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.fromDateTime(selectedTime),
+                ).then((time) {
+                  if (time != null) {
+                    selectedTime = DateTime(
+                      selectedTime.year,
+                      selectedTime.month,
+                      selectedTime.day,
+                      time.hour,
+                      time.minute,
+                    );
                   }
-                },
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Time (HH:mm)',
               ),
-            ],
-          ),
-          SizedBox(height: 10.0),
-          TextField(
-            controller: _titleController,
-            maxLines: 1,
-            decoration: InputDecoration(
-              labelText: 'Başlık',
-              border: OutlineInputBorder(),
-              filled: true,
-              fillColor: Colors.grey.shade100,
             ),
-          ),
-          SizedBox(height: 10.0),
-          TextField(
-            controller: _descController,
-            maxLines: 5,
-            decoration: InputDecoration(
-              labelText: 'Açıklama',
-              border: OutlineInputBorder(),
-              filled: true,
-              fillColor: Colors.grey.shade100,
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final title = _eventTitleController.text;
+                final notes = _eventNotesController.text;
+                if (title.isNotEmpty && notes.isNotEmpty) {
+                  onEventAdded(title, notes, selectedTime);
+                  Navigator.pop(context);
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Error'),
+                      content: Text('Title and notes cannot be empty.'),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('OK'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              child: Text('Add Event'),
             ),
-          ),
-          SizedBox(height: 10.0),
-          TextField(
-            controller: _reminderTimeController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: '.. dakika önce Hatırlat'
-                  ' boş veya 0 girilirse zamanında hatırlatılır',
-              border: OutlineInputBorder(),
-              filled: true,
-              fillColor: Colors.grey.shade100,
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              _addEvent();
-            },
-            child: const Text("Kaydet"),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
-
-  void _addEvent() async {
-    final title = _titleController.text;
-    final description = _descController.text;
-    final reminderTimeInMinutes =
-        int.tryParse(_reminderTimeController.text) ?? 0;
-
-    if (title.isEmpty) {
-      print('Başlık boş olamaz');
-      return;
-    }
-
-    final eventDateTime = DateTime(
-      _selectedDate.year,
-      _selectedDate.month,
-      _selectedDate.day,
-      _selectedTime.hour,
-      _selectedTime.minute,
-    );
-
-    final reminderDateTime =
-    eventDateTime.subtract(Duration(minutes: reminderTimeInMinutes));
-
-    await FirebaseFirestore.instance.collection('events').add({
-      "title": title,
-      "description": description,
-      "date": Timestamp.fromDate(eventDateTime),
-      "reminder": Timestamp.fromDate(reminderDateTime),
-    });
-
-    await _scheduleNotification(
-      reminderDateTime,
-      title,
-      description,
-    );
-
-    if (mounted) {
-      Navigator.pop<bool>(context, true);
-    }
-  }
-
-  Future<void> _scheduleNotification(
-      DateTime scheduledDateTime,
-      String title,
-      String description,
-      ) async {
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      'event_reminder',
-      'Event Reminder',
-      importance: Importance.max,
-      priority: Priority.high,
-      playSound: true,
-      showWhen: true,
-    );
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
-      android: androidPlatformChannelSpecifics,
-      iOS: iOSPlatformChannelSpecifics,
-    );
-
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-      0,
-      title,
-      description,
-      tz.TZDateTime.from(scheduledDateTime, tz.local),
-      platformChannelSpecifics,
-      androidAllowWhileIdle: true,
-      uiLocalNotificationDateInterpretation:
-      UILocalNotificationDateInterpretation.absoluteTime,
-    );
-  }
 }
+
